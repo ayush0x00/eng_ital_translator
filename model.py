ee9b95ec8195dbe920aaab863ee9f9e085b4eb13
Ayush Tiwari <ayushtiwari5747462@gmail.com>
2023-07-02 16:29:31 +0530

    layerNorm and feedForward

--- model.py
+++ model.py
@@ -42,3 +42,28 @@ class PositionalEncoding(nn.Module):
     def forward(self,x): # pe have batch size as the first dim. (1,seq_len,d_model)
         x = x + (self.positional_encoding[:,:x.shape[1],:]).requires_grad(False)
         return self.dropout(x)
+
+class LayerNormalization(nn.Module):
+    def __init__(self, eps:float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha=nn.Parameter(torch.ones(1)) # multiplied while layer normalizing
+        self.beta=nn.Parameter(torch.zeros(1)) # added while layer normalizing
+
+    def forward(self,x):
+        mean = x.mean(dim=-1,keepdim=True)
+        std = x.std(dim=-1,keepdim=True)
+        return self.alpha * (x-mean)/(std+self.eps) + self.beta
+    
+class FeedForward(nn.Module):
+    def __init__(self,d_model:int,d_ff:int,dropout:float ) -> None:
+        super().__init__()
+        # IP and OP both dim are (1,seq_len,d_model)
+        # inner layer dim is (1,seq_len,d_ff)
+        self.linear1 = nn.Linear(d_model,d_ff)
+        self.linear2=nn.Linear(d_ff,d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self,x):
+        return self.linear2(self.dropout(torch.relu(self.linear1(x))))
+    
